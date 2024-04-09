048c90c510fe2ef1ce72a89f315a6758ae16f563
abbodev <abbodev@gmail.com>
2024-04-09 10:28:43 +0200

    feat: add user routes

---
+++ app/users/[...user]/page.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function User() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      User - Work in progress
+      <Link
+        href="/"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      >
+        Back to homepage
+      </Link>
+    </main>
+  );
+}
