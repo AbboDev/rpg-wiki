90b4bad993c569d6018fbe4d5eb2a05776b9edd2
abbodev <abbodev@gmail.com>
2024-04-16 23:22:51 +0200

    feat(wysiwyg): setup base editor content

--- components/Editor/index.tsx
+++ components/Editor/index.tsx
@@ -4,11 +4,30 @@ import * as Puck from '@measured/puck';
 import '@measured/puck/puck.css';
 import { ComponentProps } from 'react';
 import * as components from './Components';
+import { Heading } from '@/components/Heading';
 
 interface Props extends Omit<ComponentProps<typeof Puck.Puck>, 'config'> {}
 
 const config: Puck.Config = {
   components,
+
+  root: {
+    fields: {
+      title: { type: 'text' },
+      description: { type: 'textarea' },
+    },
+    render: ({ children, description, title }) => {
+      return (
+        <section className="space-y-2">
+          <Heading>{title}</Heading>
+
+          {description && <p className="whitespace-pre-line">{description}</p>}
+
+          {children}
+        </section>
+      );
+    },
+  },
 };
 
 // Render Puck editor
