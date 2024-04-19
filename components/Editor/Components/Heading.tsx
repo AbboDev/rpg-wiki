fe2eff08273546ca6deb3c32299e05e3005ec9a8
abbodev <abbodev@gmail.com>
2024-04-19 22:35:43 +0200

    feat(wysiwyg): added rank field to heading and apply defaults

--- components/Editor/Components/Heading.tsx
+++ components/Editor/Components/Heading.tsx
@@ -3,11 +3,28 @@ import { Heading as BaseHeading } from "@/components/Heading";
 
 export const Heading: ComponentConfig = {
   fields: {
-    children: {
+    text: {
       type: 'text',
+      label: 'Text',
+    },
+    rank: {
+      type: 'radio',
+      label: 'Rank',
+      options: [
+        { label: 'H1', value: 1 },
+        { label: 'H2', value: 2 },
+        { label: 'H3', value: 3 },
+        { label: 'H4', value: 4 },
+        { label: 'H5', value: 5 },
+        { label: 'H6', value: 6 },
+      ],
     },
   },
-  render: ({ children }) => {
-    return <BaseHeading rank={2}>{children}</BaseHeading>;
+  defaultProps: {
+    text: 'Heading',
+    size: 2,
+  },
+  render: ({ text, rank }) => {
+    return <BaseHeading rank={rank}>{text}</BaseHeading>;
   },
 };
