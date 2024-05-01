c06f0ad78b93b7a55de89b4b6b13b9e95e991423
abbodev <abbodev@gmail.com>
2024-05-01 15:56:48 +0200

    feat: add Google provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,12 @@
 import NextAuth from 'next-auth';
 
 import GitHub from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 
 import type { NextAuthConfig } from 'next-auth';
 
 export const config = {
-  providers: [GitHub],
+  providers: [GitHub, Google],
   basePath: '/auth',
 } satisfies NextAuthConfig;
 
