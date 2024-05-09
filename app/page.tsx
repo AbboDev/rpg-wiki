c2c57e20cdd948404d158fe02cabc2283b8bc96a
abbodev <abbodev@gmail.com>
2024-05-09 22:21:44 +0200

    feat: add instagram login
    Actually, the Auth.js library block Instagram access_token response since it's missing the required
    property "token_type"

--- app/page.tsx
+++ app/page.tsx
@@ -136,6 +136,19 @@ export default async function Home() {
                 Signin with Discord
               </button>
             </form>
+            <form
+              action={async () => {
+                'use server';
+                await signIn('instagram');
+              }}
+            >
+              <button
+                type="submit"
+                className="rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              >
+                Signin with Instagram
+              </button>
+            </form>
           </>
         ) : (
           <>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ import Reddit from 'next-auth/providers/reddit';
 import Twitter from 'next-auth/providers/twitter';
 import Facebook from 'next-auth/providers/facebook';
 import Discord from 'next-auth/providers/discord';
+import Instagram from 'next-auth/providers/instagram';
 
 import type { NextAuthConfig } from 'next-auth';
 
@@ -23,6 +24,7 @@ export const config = {
     Twitter,
     Facebook,
     Discord,
+    Instagram,
   ],
   basePath: '/auth',
   callbacks: {
